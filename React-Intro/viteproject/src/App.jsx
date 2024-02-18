81ef7dd96071ba12c58a6c27ec080bee9d00bf57
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-02-18 08:51:44 +0000

    try to create react project with the help of vite library

---
+++ React-Intro/viteproject/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad expedita modi, labore, dolores veritatis facere id vero inventore enim commodi doloremque delectus vitae cumque! Laudantium itaque nostrum excepturi neque vero?</p>
+    </>
+  )
+}
+
+export default App
